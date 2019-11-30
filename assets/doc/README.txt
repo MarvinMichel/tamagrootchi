f79fb9b2aa06596d8da448744b56c47db067c098
Martian Code <mmhsernee@gmail.com>
2019-11-30 17:39:15 +0100

    Add files via upload

---
+++ assets/doc/README.txt
@@ -0,0 +1,7 @@
+Bronnenlijst:
+- Marvel (Guardians of the Galaxy) . (2015). Guardians Of The Galaxy ❤ 4K HD Desktop Wallpaper for 4K Ultra HD TV • Wide & Ultra Widescreen Displays • Dual Monitor Desktops • Tablet • Smartphone • Mobile Devices. Photograph. Retrieved from http://wallpaperswide.com/guardians_of_the_galaxy_3-wallpapers.html
+- Attention Required! | Cloudflare. (n.d.). Photograph. Retrieved from https://www.artstation.com/artwork/4Bz28
+
+Animaties en groot vector images van Groot zijn zelfgemaakt met Adobe After Effects en Adobe Illustrator respectievelijk. Auteursrechten van Guardians of the Galaxy en character 'Groot' gaan naar Marvel Studio™ en Disney™. De Marvel Comics die geladen worden via fetch API behoren tot Marvel Comics™ (© 2014 Marvel).
+
+© Copyright 2019, Marvin Sernee.
---
+++ assets/js/groot.js
@@ -0,0 +1,118 @@
+//Audiobestanden
+const song = new Audio('./assets/sfx/mrBlueSky.mp3');
+const greeting = new Audio('./assets/sfx/iAmGroot.mp3');
+
+//HTML elementen die meerdere keren worden gebruikt
+const animContainer = $('.animContainer').get(0);
+const fetchContainer = $('.fetchContainer').get(0);
+const buttonsBig = $('.button--big');
+
+//Datum en uren van het moment
+const today = new Date();
+const hours = today.getHours();
+
+//Houd bij hoevaak de 'Dance' knop is ingedrukt
+let btnPress = 0;
+
+//Veranderen van achtergrond op basis van tijd
+if (hours >= 21 || hours <= 5) {
+    $('body').addClass('nightTime');
+}
+
+// Array met data objecten voor animaties
+const animData = [{
+    title: 'Grow',
+    container: animContainer,
+    renderer: 'svg',
+    autoplay: false,
+    loop: false,
+    path: './assets/js/grow.json'
+}, {
+    title: 'Shrink',
+    container: animContainer,
+    renderer: 'svg',
+    autoplay: false,
+    loop: false,
+    path: './assets/js/shrink.json'
+}, {
+    title: 'Dance',
+    container: animContainer,
+    renderer: 'svg',
+    autoplay: false,
+    loop: 27,
+    path: './assets/js/dance.json'
+}];
+
+//Laat 'Groot' groeien
+const growGroot = () => {
+    groot.destroy();
+    groot = bodymovin.loadAnimation(animData[0]);
+    groot.play();
+    btnEnable();
+}
+
+//Laat 'Groot' gedag zeggen
+const iAmGroot = () => {
+    greeting.play();
+}
+
+//Laat 'Groot' krimpen
+const shrinkGroot = () => {
+    song.pause();
+    song.currentTime = 0;
+    groot.destroy();
+    groot = bodymovin.loadAnimation(animData[1]);
+    groot.play();
+    btnEnable();
+}
+
+//Laat 'Groot' dansen
+const danceGroot = () => {
+    if (btnPress % 2 === 0 || btnPress === 0) {
+        song.play();
+        groot.destroy();
+        groot = bodymovin.loadAnimation(animData[2]);
+        groot.play();
+    } else {
+        song.pause();
+        groot.pause();
+    }
+    btnPress++;
+}
+
+//Haal informatie over 'Groot' binnen via Wikipedia en dump het in een section
+const fetchGroot = () => {
+    const url = 'https://en.wikipedia.org/w/api.php?&origin=*&action=opensearch&search=Groot&limit=5"';
+    fetch(url)
+        .then((res) => {
+            return res.json();
+        }).then((data) => {
+            document.querySelector('h2').textContent = 'About Groot';
+            document.querySelector('p').textContent = data[2][0];
+            $(fetchContainer).css('background-color', 'rgba(30, 27, 56, 0.45)');
+        })
+        .catch((error) => {
+            console.log('Er ging iets mis met de fetch...');
+        });
+}
+
+//Schakel knoppen in of uit
+const btnEnable = () => {
+    for (let el of buttonsBig) {
+        if (el.disabled) {
+            el.disabled = false;
+        } else {
+            el.disabled = true;
+        }
+    }
+}
+
+//Geeft een vaste waarde aan de container, zodat het begin stadium een boompje is
+let groot = bodymovin.loadAnimation(animData[0]);
+
+//Events
+document.querySelector('.title').addEventListener('dblclick', iAmGroot);
+// animContainer.addEventListener('click', fetchGroot);
+document.querySelector('#grow').addEventListener('click', growGroot)
+document.querySelector('#shrink').addEventListener('click', shrinkGroot)
+document.querySelector('#dance').addEventListener('click', danceGroot)
