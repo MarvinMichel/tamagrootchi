b356d1961ce5d068581f241c610ec2a60ef25166
MarvinMichel <mmhsernee@gmail.com>
2020-10-09 17:43:34 +0200

    Remove unnecessary CSS file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Character from './components/Character/Character';
 import ButtonContainer from './components/ButtonContainer/ButtonContainer';
-import './App.css';
 
 const App = () => (
   <>
