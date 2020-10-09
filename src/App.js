60df74b3b01b35adb01a223252b7b3ade978b0c3
MarvinMichel <mmhsernee@gmail.com>
2020-10-09 19:28:27 +0200

    Add Three.js and 3D model

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import Header from './components/Header/Header';
+// import Header from './components/Header/Header';
 import Character from './components/Character/Character';
 import ButtonContainer from './components/ButtonContainer/ButtonContainer';
 
 const App = () => (
   <>
-    <Header />
     <Character />
     <ButtonContainer />
   </>
